b22009efa2dc40b19420b9ff07b6407e1750d7a4
jramosss <jramostod@gmail.com>
2021-07-08 14:46:13 -0300

    Ups :broom:

--- spotify.py
+++ spotify.py
@@ -6,12 +6,9 @@ import spotify_uri
 
 load_dotenv('./.env')
 
-birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
 CLIENT_ID = environ["SPOTIPY_CLIENT_ID"]
 CLIENT_SECRET = environ["SPOTIPY_CLIENT_SECRET"]
-#print(CLIENT_ID, CLIENT_SECRET)
-#CLIENT_ID = '65ac6b7644b648f0a4dea2f284e7c164'
-#CLIENT_SECRET = 'f039081dfe9a4f9c9d2568101224cf27'
+
 spotify = spotipy.Spotify(
     client_credentials_manager=SpotifyClientCredentials(CLIENT_ID, CLIENT_SECRET))
 
